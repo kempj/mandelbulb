df9938f123285823d592ed1df490949334f983c4
Jeremy Kemp <jmknsd@gmail.com>
2010-12-07 16:13:14 -0600

    more changes for I/O to marching

--- main.cpp
+++ main.cpp
@@ -6,8 +6,8 @@
 using namespace std;
 
 
-void findEdges(float &front, float &back, float &left, float &right, float &top, float &bottom);
-int iterate(float x, float y, float z);
+void findEdges(double &front, double &back, double &left, double &right, double &top, double &bottom);
+int iterate(double x, double y, double z);
 
 int main(int argc, char *argv[]){
 
@@ -15,8 +15,8 @@ int main(int argc, char *argv[]){
     char filename[20];
     const int ASIZE = 128;
     //int coords[ASIZE][ASIZE];
-    float front, back, left, right, top, bottom;
-    float deltaX, deltaY, deltaZ;
+    double front, back, left, right, top, bottom;
+    double deltaX, deltaY, deltaZ;
 
     findEdges(front, back, left, right, top, bottom);
 
@@ -25,34 +25,42 @@ int main(int argc, char *argv[]){
     deltaZ = (back - front) / ASIZE;
 
 
+    sliceOut.open("slices/1.dat");
+    if(!sliceOut.is_open()) {
+	cout << "couldn't open file\n";
+	return 0;
+    }
+
+    sliceOut << ASIZE << endl;
+    sliceOut << front << endl;
+    sliceOut << left << endl;
+    sliceOut << bottom << endl;
+    sliceOut << deltaX << endl;
+    sliceOut << deltaY << endl;
+    sliceOut << deltaZ << endl;
+
     //moving K outside to write each slice to file
-    for(int k = 0; k < ASIZE; k++) {// front - back
+    for(int k = 0; k < ASIZE;) {// front - back
+	for(int i = 0; i < ASIZE; i++) {//left -right
+	    for(int j = 0; j < ASIZE; j++) {//bottom - top
+		sliceOut <<  iterate(left + i*deltaX, bottom + j*deltaY, front + k*deltaZ);
+            }
+	    sliceOut << endl;
+	}
+	sliceOut.close();
+	k++;
 	sprintf(filename,"slices/%d.dat",k+1);
 	sliceOut.open(filename);
 	if(!sliceOut.is_open()) {
 	    cout << "couldn't open file\n";
 	    return 0;
 	}
-	sliceOut << ASIZE << endl;
-	sliceOut << front + k*deltaZ << endl;
-	for(int i = 0; i < ASIZE; i++) {//left -right
-	    // cout << i << endl;
-	    for(int j = 0; j < ASIZE; j++) {//bottom - top
-		//coords[i][j]
-		sliceOut <<  iterate(left + i*deltaX, bottom + j*deltaY, front + k*deltaZ);
-                //if(coords[i][j][k] != -1)
-                    //cout << coords[i][j][k] << ", ";
-
-            }
-	    sliceOut << endl;
-	}
-	sliceOut.close();
     }
     return 0;
 }
 
 //Calculates the outermost edges of the fractal, must be called 6 times
-void findEdges(float &front, float &back, float &left, float &right, float &top, float &bottom){
+void findEdges(double &front, double &back, double &left, double &right, double &top, double &bottom){
     //Start from edge = 2 and move in until a converging vertex is found
     back = right = top = 1.5;
     front = left = bottom = -1.5;
@@ -60,11 +68,11 @@ void findEdges(float &front, float &back, float &left, float &right, float &top,
 
 //does the actual number crunching, returns -1 if the coords diverge,
 // otherwise it returns the number of iterations
-int iterate(float Xin, float Yin, float Zin) {
-    float x = Xin;
-    float y = Yin;
-    float z = Zin;
-    float theta, phi, r;
+int iterate(double Xin, double Yin, double Zin) {
+    double x = Xin;
+    double y = Yin;
+    double z = Zin;
+    double theta, phi, r;
     int iterCount = 0;
     int n = 8;//order of the equation
 
@@ -83,5 +91,5 @@ int iterate(float Xin, float Yin, float Zin) {
         iterCount ++;
     }
 
-    return 1;// sqrt(Xin*Xin + Yin*Yin + Zin*Zin);//Future add coloring scheme.
+    return sqrt(Xin*Xin + Yin*Yin + Zin*Zin);//Future add coloring scheme.
 }
